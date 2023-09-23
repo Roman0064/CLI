3c0af85e08fdba3efc2e024b3da26921028dc14b
Roman0064 <alee93474@gmail.com>
2023-09-24 00:40:29 +0300

    1

--- contacts.js
+++ contacts.js
@@ -1,10 +1,40 @@
 const fs = require('fs/promises');
+const path = require('path');
 
-const listContacts = async () => {
-    const data = await fs.readFile('./db/contacts.json', 'utf-8');
-    return JSON.parse(data);
-}
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const getAll = async () => {
+    try{
+        const data = await fs.readFile(contactsPath, 'utf-8');
+        const allContacts = JSON.parse(data);
+        return allContacts;
+    } catch (error){
+        console.log(error.message)
+    };
+};
+
+const getContactById = async (id) => {
+    try{
+        const contact = await getAll();
+        const contactById = contact.find(item => item.id === id);
+        return contactById || null;
+    } catch (error){
+        console.log(error.message);
+    };
+};
+
+function removeContact(contactId) {
+    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+};
+  
+function addContact(name, email, phone) {
+    // ...твій код. Повертає об'єкт доданого контакту. 
+};
+
 
 module.exports = {
-    listContacts,
+    getAll,
+    getContactById,
+    removeContact,
+    addContact,
 };
--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 
     switch(action){
         case "list":
-            const allContacts = await contacts.listContacts();
-            console.log(allContacts);
-            break;
+            const allContacts = await contacts.getAll();
+            console.log("List of Contacts:");
+            console.table(allContacts);
+            return allContacts;
+
+        case "getById":
+            const contact = await contacts.getContactById(id);
+            console.log("Contact Details:");
+            console.table([contact]);
+            return contact;
+
+        case "addContact":
+            const newContact = await contacts.addContact(name, email, phone);
+            console.log("Removed Contact:");
+            console.log([newContact]);
+            return newContact;
+
+        case "removeContact":
+            const removedContact = await contacts.removeContact(id);
+            console.log("New Contact:");
+            console.log([removedContact]);
+            return removedContact;
     };
 
 };
 
-invokeAction({action: 'list'});
+// invokeAction({action: 'list'});
+// invokeAction({action: 'getById', id: "05olLMgyVQdWRwgKfg5J6"});
+// invokeAction({action: 'removeContact', id: "05olLMgyVQdWRwgKfg5J6"});
+// invokeAction({action: 'addContacts', id: "05olLMgyVQdWRwgKfg5J6"});
